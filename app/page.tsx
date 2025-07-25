beae6863f2a415e91b07fe37b027f196d209e4ff
Sergio Parra <parranopolis00@gmail.com>
2025-07-25 17:19:48 -0600

    Fix minimal working version for deployment

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,11 @@
-// import Image from "next/image";
-import Head from 'next/head'
-import Hero from "./components/Hero";
-import Features from "./components/Features";
-import Footer from "./components/Footer";
-import Pricing from "./components/Pricing";
-import Image from 'next/image';
-import CTA from './components/CTA';
+// // import Image from "next/image";
+// import Head from 'next/head'
+// import Hero from "./components/Hero";
+// import Features from "./components/Features";
+// import Footer from "./components/Footer";
+// import Pricing from "./components/Pricing";
+// import Image from 'next/image';
+// import CTA from './components/CTA';
 
 export default function Home() {
     return (
